03dae6e5aeef7d590e714872fd4d29330a6e7e15
xxCherry <aazetthr@gmail.com>
2020-05-10 01:45:55 +0300

    Oops.. :D

--- oyasumi/Controllers/BanchoController.cs
+++ oyasumi/Controllers/BanchoController.cs
@@ -65,8 +65,6 @@ namespace oyasumi.Controllers
                 body.Position = 0;
                 ms.Position = 0;
 
-                Console.WriteLine("A\n\n\n\n\n\n\nOOOOOO.");
-
                 return await FileAsync(ms);
             }
             else
