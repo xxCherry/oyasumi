1598f3d7c81bf1906d05a661138e0b7f24a29aa4
Keijia <45855057+Airkek@users.noreply.github.com>
2021-01-05 22:14:13 +0300

    Gulag Database Merger (#11)
    * Gulag Database Merger

---
+++ gulagDatabaseMerger/Database/Models/GulagBeatmap.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using oyasumi.Enums;
+
+namespace gulagDatabaseMerger.Database.Models
+{
+
+    [Table("maps")]
+    public class GulagBeatmap
+    {
+        [Required]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required] [Column("md5")] public string Checksum { get; set; }
+        [Required] [Column("status")] public RankedStatus Status { get; set; }
+        [Required] [Column("set_id")] public int SetId { get; set; }
+        [Required] [Column("artist")] public string Artist { get; set; }
+        [Required] [Column("title")] public string Title { get; set; }
+        [Required] [Column("version")] public string Version { get; set; }
+        [Required] [Column("creator")] public string Creator { get; set; }
+        [Required] [Column("plays")] public int PlayCount { get; set; }
+        [Required] [Column("passes")] public int PassCount { get; set; }
+        [Required] [Column("frozen")] public bool Frozen { get; set; }
+        [Required] [Column("bpm")] public float BPM { get; set; }
+        [Required] [Column("cs")] public float CS { get; set; }
+        [Required] [Column("ar")] public float AR { get; set; }
+        [Required] [Column("od")] public float OD { get; set; }
+        [Required] [Column("hp")] public float HP { get; set; }
+        [Required] [Column("diff")] public float SR { get; set; }
+    }
+}
---
+++ gulagDatabaseMerger/Database/Models/GulagScore.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gulagDatabaseMerger.Database.Models
+{
+    [Table("scores_vn")]
+    public class GulagScore
+    {
+        [Required]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required] [Column("map_md5")] public string BeatmapChecksum { get; set; }
+        [Required] [Column("userid")] public int UserId { get; set; }
+        [Required] [Column("score")] public int Score { get; set; }
+        [Required] [Column("status")] public int Completed { get; set; }
+        [Required] [Column("max_combo")] public int MaxCombo { get; set; }
+        [Required] [Column("mods")] public int Mods { get; set; }
+        [Required] [Column("n300")] public int Count300 { get; set; }
+        [Required] [Column("n100")] public int Count100 { get; set; }
+        [Required] [Column("n50")] public int Count50 { get; set; }
+        [Required] [Column("ngeki")] public int CountGeki { get; set; }
+        [Required] [Column("nkatu")] public int CountKatu { get; set; }
+        [Required] [Column("nmiss")] public int CountMiss { get; set; }
+        [Required] [Column("play_time")] public DateTime Time { get; set; } 
+        [Required] [Column("mode")] public int PlayMode { get; set; }
+        [Required] [Column("acc")] public double Accuracy { get; set; }
+        [Required] [Column("pp")] public float Performance { get; set; }
+    }
+    
+    [Table("scores_rx")]
+    public class GulagRelaxScore
+    {
+        [Required]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required] [Column("map_md5")] public string BeatmapChecksum { get; set; }
+        [Required] [Column("userid")] public int UserId { get; set; }
+        [Required] [Column("score")] public int Score { get; set; }
+        [Required] [Column("status")] public int Completed { get; set; }
+        [Required] [Column("max_combo")] public int MaxCombo { get; set; }
+        [Required] [Column("mods")] public int Mods { get; set; }
+        [Required] [Column("n300")] public int Count300 { get; set; }
+        [Required] [Column("n100")] public int Count100 { get; set; }
+        [Required] [Column("n50")] public int Count50 { get; set; }
+        [Required] [Column("ngeki")] public int CountGeki { get; set; }
+        [Required] [Column("nkatu")] public int CountKatu { get; set; }
+        [Required] [Column("nmiss")] public int CountMiss { get; set; }
+        [Required] [Column("play_type")] public DateTime Time { get; set; }
+        [Required] [Column("mode")] public int PlayMode { get; set; }
+        [Required] [Column("acc")] public double Accuracy { get; set; }
+        [Required] [Column("pp")] public float Performance { get; set; }
+    }
+}
